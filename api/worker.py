df0eb02da46a3b224ba76be3a8547d96ff20e97d
ahsanbaig12 <157407849+ahsanbaig12@users.noreply.github.com>
2026-01-04 00:16:12 +0000

    Add API worker and clean up cache

--- api/worker.py
+++ api/worker.py
@@ -1,10 +1,9 @@
 import os
+from rq import Worker, Queue
 from redis import Redis
-from rq import Worker, Queue, Connection
 
-REDIS_URL = os.environ.get("REDIS_URL", "redis://localhost:6379")
+REDIS_URL = os.getenv("REDIS_URL", "redis://default:NNctLTQPpTrriJGYWKzcDBSdsQPDIEmy@re-dis.railway.internal:6379")
 redis_conn = Redis.from_url(REDIS_URL)
-with Connection(redis_conn):
-    q = Queue()
-    w = Worker([q])
-    w.work()
+queue = Queue("default", connection=redis_conn)
+worker = Worker([queue], connection=redis_conn)
+worker.work()
